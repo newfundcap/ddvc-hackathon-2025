09e08b8a1107a706b95917322ed4fc7c73a84b20
Antoine Latrille <antoine.latrille@gmail.com>
2025-01-15 15:48:44 +0100

    fix json for companies

--- models.py
+++ models.py
@@ -1,15 +1,21 @@
+import json
+
 from peewee import *
 from datetime import datetime
 from config import config
+from playhouse.pool import PooledPostgresqlExtDatabase
+from playhouse.postgres_ext import BinaryJSONField, PostgresqlExtDatabase
 
 # Database Connection
-db = PostgresqlDatabase(
+db = PostgresqlExtDatabase(
     config.db_name,
     user=config.db_user,
     password=config.db_password,
     host=config.db_host,
     port=config.db_port,
 )
+
+
 
 # Models
 class BaseModel(Model):
@@ -18,14 +24,13 @@ class BaseModel(Model):
 
 class Company(BaseModel):
     id = AutoField()
-    name = CharField()
+    name = CharField(null=False)
     country = CharField()
     sector = CharField()
-    team = TextField()  # JSON data
     contact = CharField()
     funding_stage = CharField()
     creation_date = DateField()
-    investors = TextField()  # JSON data
+    investors = BinaryJSONField(default='[]')
     revenue = FloatField()
     revenue_growth = FloatField()
     total_amount_raised = FloatField()
@@ -36,15 +41,15 @@ class Company(BaseModel):
     pdl_id = CharField()
     full_time_employees = IntegerField()
     full_time_employees_growth = FloatField()
-    created_at = DateTimeField(default=datetime.utcnow)
-    updated_at = DateTimeField(default=datetime.utcnow)
+    created_at = DateTimeField(default=datetime.now)
+    updated_at = DateTimeField(default=datetime.now)
 
 class People(BaseModel):
     id = AutoField()
     first_name = CharField()
     last_name = CharField()
     education = CharField()
-    past_companies = TextField()  # JSON data
+    past_companies = BinaryJSONField(default='{}')
     linkedin = CharField()
     github = CharField()
     previous_founded_companies_count = IntegerField()
@@ -58,8 +63,8 @@ class CompanyPeople(BaseModel):
 class Filter(BaseModel):
     id = AutoField()
     name = CharField()
-    created_at = DateTimeField(default=datetime.utcnow)
-    updated_at = DateTimeField(default=datetime.utcnow)
+    created_at = DateTimeField(default=datetime.now)
+    updated_at = DateTimeField(default=datetime.now)
 
 class FilterCompany(BaseModel):
     id = AutoField()
@@ -69,15 +74,15 @@ class FilterCompany(BaseModel):
 class Ranker(BaseModel):
     id = AutoField()
     name = CharField()
-    created_at = DateTimeField(default=datetime.utcnow)
-    updated_at = DateTimeField(default=datetime.utcnow)
+    created_at = DateTimeField(default=datetime.now)
+    updated_at = DateTimeField(default=datetime.now)
 
 class RankerCompany(BaseModel):
     id = AutoField()
     company = ForeignKeyField(Company, backref='rankers')
     ranker = ForeignKeyField(Ranker, backref='companies')
     score = FloatField()
-    created_at = DateTimeField(default=datetime.utcnow)
-    updated_at = DateTimeField(default=datetime.utcnow)
+    created_at = DateTimeField(default=datetime.now)
+    updated_at = DateTimeField(default=datetime.now)
 
 
