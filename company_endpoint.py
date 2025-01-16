04221a4e4036716489b6666a32ce4fdbda0e9554
Antoine Latrille <antoine.latrille@gmail.com>
2025-01-16 13:27:11 +0100

    first raw version of the get companies

--- company_endpoint.py
+++ company_endpoint.py
@@ -1,12 +1,15 @@
+import json
 from http.client import HTTPException
 from typing import Optional
 
 from fastapi import APIRouter, BackgroundTasks
+from peewee import JOIN, prefetch
 from playhouse.shortcuts import model_to_dict
 
+import json_utils
 import processing
 
-from models import Company, RankerCompany
+from models import Company, RankerCompany, Filter, FilterCompany
 from pydantic import BaseModel
 
 
@@ -37,12 +40,31 @@ async def create_company(req: CreateCompanyRequest, background_tasks: Background
         raise HTTPException(status_code=400, detail=str(e))
 
 
+
 @router.get("/")
-async def list_companies(sort_by: str = None, ranker_score: float = None):
-    # TODO write a correct list / search endpoint
-    query = Company.select()
-    if ranker_score:
-        query = query.join(RankerCompany).where(RankerCompany.score == ranker_score)
-    if sort_by:
-        query = query.order_by(getattr(Company, sort_by))
-    return [model_to_dict(company) for company in query]
+async def list_companies():
+    # companies_q = Company.select()
+    # filter_company = FilterCompany.select()
+    # ranker_company = RankerCompany.select()
+    
+    companies = Company.select().order_by(Company.created_at.desc())
+
+
+    # query = query.join(RankerCompany).where(RankerCompany.score == ranker_score)
+    # if sort_by:
+    #     query = query.order_by(getattr(Company, sort_by))
+
+    resp = []
+    for company in companies:
+        company_details = model_to_dict(company)
+        team = [people.people for people in company.employees]
+        people_details = [model_to_dict(t)for t in team]
+        rankers_results = []
+        filters = []
+        company_details['team'] = people_details
+        company_details['rankers'] = rankers_results
+        company_details['filters'] = filters
+
+        resp.append(company_details)
+
+    return resp
