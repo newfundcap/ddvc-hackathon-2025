1952d8d0d51322b6abfbaad574372f58d43e10f3
Priscilla <148735058+priscillachi@users.noreply.github.com>
2025-01-16 14:15:52 +0100

    populate rankers

--- ranker_endpoint.py
+++ ranker_endpoint.py
@@ -1,14 +1,38 @@
 from http.client import HTTPException
+from typing import Optional, Any, Dict, List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, BackgroundTasks
+from peewee import prefetch
 from playhouse.shortcuts import model_to_dict
 
-from models import Ranker
+from models import Ranker, RankerCompany
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix='/rankers',
     tags=['rankers']
 )
+
+
+class RankerCompanyResponse(BaseModel):
+    id: int
+    name: str
+    description: str
+    score: float
+    category: str
+    justification: str
+    warnings: str
+
+
+class RankerResponse(BaseModel):
+    id: int
+    name: str
+    description: str
+
+    companies: List[RankerCompanyResponse]
+
+    class Config:
+        orm_mode = True
 
 
 @router.post("/")
@@ -28,3 +52,34 @@ async def delete_ranker(ranker_id: int):
         raise HTTPException(status_code=404, detail="Ranker not found")
     ranker_obj.delete_instance()
     return {"detail": "Ranker deleted"}
+
+
+@router.get("/")
+async def list_rankers() -> list[RankerResponse]:
+    rankers = prefetch(
+        Ranker.select().order_by(Ranker.created_at.desc()),
+        RankerCompany.select(),
+    )
+    resp = []
+    for ranker in rankers:
+        ranker_response = RankerResponse(
+            id=ranker.id,
+            name=ranker.name,
+            description=ranker.description,
+
+            companies=[
+                RankerCompanyResponse(
+                    id=r_c.ranker.id,
+                    name=r_c.ranker.name,
+                    description=r_c.ranker.description,
+                    score=r_c.score,
+                    category=r_c.category,
+                    justification=r_c.justification,
+                    warnings=r_c.warnings,
+                ) for r_c in ranker.rankers
+            ],  # Assuming companies data to populate here
+        )
+
+        resp.append(ranker_response)
+
+    return resp
