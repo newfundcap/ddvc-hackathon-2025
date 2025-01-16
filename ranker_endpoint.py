44bad71d9f156b95f240e29f24cf47c67941133e
Priscilla <148735058+priscillachi@users.noreply.github.com>
2025-01-16 13:41:22 +0100

    added stuff to ranker_endpoint

--- ranker_endpoint.py
+++ ranker_endpoint.py
@@ -1,7 +1,30 @@
+from http.client import HTTPException
+
 from fastapi import APIRouter
+from playhouse.shortcuts import model_to_dict
+
+from models import Ranker
 
 router = APIRouter(
     prefix='/rankers',
     tags=['rankers']
 )
 # TODO implement the correct endpoint
+
+@router.post("/")
+async def create_filter(ranker_data: dict):
+    # TODO implement the correct endpoint
+    try:
+        ranker_obj = Ranker.create(**ranker_data)
+        return model_to_dict(ranker_obj)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.delete("/{ranker_id}")
+async def delete_ranker(ranker_id: int):
+    ranker_obj = Ranker.get_or_none(id=ranker_id)
+    if not ranker_obj:
+        raise HTTPException(status_code=404, detail="Filter not found")
+    ranker_obj.delete_instance()
+    return {"detail": "Filter deleted"}
