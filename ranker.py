d1d0d9257868956d0b7d8ca2f66f27a6133f2ae9
Priscilla <148735058+priscillachi@users.noreply.github.com>
2025-01-16 12:01:15 +0100

    changes

--- ranker.py
+++ ranker.py
@@ -52,6 +52,8 @@ def rank_company(company: Company, people: People):
 
     startup_match_info = completion.choices[0].message.content.strip()
 
+    #db.connect()
+
     ranker_company = RankerCompany.select().where(RankerCompany.company.id == startup_match_info.get("startup-id")).get()
     ranker_company.score = int(startup_match_info.get("score"))
     ranker_company.category = startup_match_info.get("category")
@@ -59,6 +61,8 @@ def rank_company(company: Company, people: People):
     ranker_company.warnings = startup_match_info.get("warnings")
 
     ranker_company.save()
+
+    #db.close()
 
 
 def delete_all_files():
