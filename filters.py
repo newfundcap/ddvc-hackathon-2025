089b6c8df04f15f85b2213f19c058a86f73702d0
Priscilla <148735058+priscillachi@users.noreply.github.com>
2025-01-16 13:34:03 +0100

    updated filtering funciton

--- filters.py
+++ filters.py
@@ -6,8 +6,11 @@ from models import Company, People
 ## team sera un json data
 ## refaire le teams pour ameliorer la recherche
 
+'''
+Example of the settings for filters
+
 settings = {
-    "country": ["France", "italy"],
+    "country": ["France", "Italy"],
     "sector": ["Health", "Food", "Tech", "Energy", "Finance", "Education", "Transport"],
     "funding_stage": ["Pre-seed ", "Seed", "Series A"],
     "min_creation_date": "2020-01-01",
@@ -15,24 +18,25 @@ settings = {
     "total_amount_raised": "1000000",
     "revenue": "1000000",
 }
+'''
 
 
 def apply_filters(company: Company, settings: dict, team: List[People]):
     if "country" in settings and len(settings["country"]) > 0:
-        valid_values = [v.lower() for v in settings["country"]]
-        country = company.country.lower()
+        valid_values = [v.lower().strip() for v in settings["country"]]
+        country = company.country.lower().strip()
         if country not in valid_values:
             return False
 
     if "sector" in settings and len(settings["sector"]) > 0:
-        valid_values = [v.lower() for v in settings["sector"]]
-        sector = company.sector.lower()
+        valid_values = [v.lower().strip() for v in settings["sector"]]
+        sector = company.sector.lower().strip()
         if sector not in valid_values:
             return False
 
     if "funding_stage" in settings and len(settings["funding_stage"]) > 0:
-        valid_values = [v.lower() for v in settings["funding_stage"]]
-        funding_stage = company.funding_stage.lower()
+        valid_values = [v.lower().strip() for v in settings["funding_stage"]]
+        funding_stage = company.funding_stage.lower().strip()
         if funding_stage not in valid_values:
             return False
 
@@ -68,30 +72,6 @@ def apply_filters(company: Company, settings: dict, team: List[People]):
 
 
 def main():
-    company = Company(
-        "name",
-        "France",
-        "Health",
-        ["frf", "gfgf", "rgrg"],
-        "contact",
-        "seed",
-        "2025-01-01",
-        "investors",
-        "1000000000",
-        "0.1",
-        "100000000",
-        "description",
-        "website",
-        "linkedin",
-        "harmonic_id",
-        "pdl_id",
-        "10",
-        "0.1",
-    )
-
-    
-
-    print("Hello from filter.py")
 
 
 if __name__ == "__main__":
